7a20ac9c866c86cc1fcf21fd46363f64b94f6516
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-06 10:03:22 +0100

    IRepository

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Repository/Generic/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace AssemblyCookieRecipe.Repository.Generic
+{
+    //CRUD
+    public interface IRepository<T, ID>
+    {
+        T Presist(T value);
+        List<T> GetAll();
+        T GetById(ID id);
+        T Update(T value);
+        void DeleteById(ID id);
+    }
+}
