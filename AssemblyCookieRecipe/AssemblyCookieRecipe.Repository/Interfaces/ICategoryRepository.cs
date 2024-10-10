d876a4934f88b1008950c9ff5ab49ab66c4f9efc
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-10 21:05:36 +0100

    Icategory repository

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Repository/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using AssemblyCookieRecipe.Model;
+using AssemblyCookieRecipe.Repository.Generic;
+
+namespace AssemblyCookieRecipe.Repository.Interfaces
+{
+    public interface ICategoryRepository : IRepository<Category, int>
+    {
+    }
+}
