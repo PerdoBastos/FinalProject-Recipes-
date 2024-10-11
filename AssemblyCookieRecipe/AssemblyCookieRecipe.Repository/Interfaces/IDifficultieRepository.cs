14b0eca8f0e418a7cf13281913cdbf526b4cf948
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-11 20:55:33 +0100

    Idifficultierepository

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Repository/Interfaces/IDifficultieRepository.cs
@@ -0,0 +1,10 @@
+﻿using AssemblyCookieRecipe.Model;
+using AssemblyCookieRecipe.Repository.Generic;
+
+namespace AssemblyCookieRecipe.Repository.Interfaces
+{
+    public interface IDifficultieRepository : IRepository<Difficultie, int>
+    {
+
+    }
+}
