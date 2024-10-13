72db9f8983d48e1ab0c801983da90bcb38ce30ea
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-13 08:19:29 +0100

    IUser repository

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using AssemblyCookieRecipe.Model;
+using AssemblyCookieRecipe.Repository.Generic;
+
+namespace AssemblyCookieRecipe.Repository.Interfaces
+{
+    public interface IUserRepository : IRepository<User, int>
+    {
+
+    }
+}
