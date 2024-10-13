d229c500bc5cc3f5d0b1a4b2e91e592cf27aaf12
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-13 08:19:13 +0100

    Imeasure repository

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Repository/Interfaces/IMeasureRepository.cs
@@ -0,0 +1,9 @@
+﻿using AssemblyCookieRecipe.Model;
+using AssemblyCookieRecipe.Repository.Generic;
+
+namespace AssemblyCookieRecipe.Repository.Interfaces
+{
+    public interface IMeasureRepository : IRepository<Measure, int>
+    {
+    }
+}
