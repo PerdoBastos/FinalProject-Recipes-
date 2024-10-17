1e5aaac3f443c763b7a061962d0e036046c2a0ce
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-17 18:56:40 +0100

    difficultie service

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Service/Implementation/DifficultieService.cs
@@ -0,0 +1,40 @@
+﻿using AssemblyCookieRecipe.Model;
+using AssemblyCookieRecipe.Repository.Interfaces;
+using AssemblyCookieRecipe.Service.Interfaces;
+
+namespace AssemblyCookieRecipe.Service.Implementation
+{
+    public class DifficultieService : IDifficultieService
+    {
+        private readonly IDifficultieRepository _repository;
+        public DifficultieService(IDifficultieRepository difficultieRepository)
+        {
+            _repository = difficultieRepository;
+        }
+
+        public void DeleteById(int id)
+        {
+            _repository.DeleteById(id);
+        }
+
+        public List<Difficultie> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Difficultie GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public Difficultie Presist(Difficultie difficultie)
+        {
+            return _repository.Presist(difficultie);
+        }
+
+        public Difficultie Update(Difficultie difficultie)
+        {
+            return _repository.Update(difficultie);
+        }
+    }
+}
