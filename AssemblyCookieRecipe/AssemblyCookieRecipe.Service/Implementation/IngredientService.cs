a19290ab5f767d73b4d927cb90eefa5923c5abd3
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-18 20:05:24 +0100

    ingredient service

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Service/Implementation/IngredientService.cs
@@ -0,0 +1,42 @@
+﻿using AssemblyCookieRecipe.Model;
+using AssemblyCookieRecipe.Repository.Interfaces;
+using AssemblyCookieRecipe.Service.Interfaces;
+
+namespace AssemblyCookieRecipe.Service.Implementation
+{
+    //O que é
+    public class IngredientService : IIngredientService
+    {
+        //O que tem
+        private readonly IIngredientRepository _repository;
+        public IngredientService(IIngredientRepository ingredientRepository)
+        {
+            _repository = ingredientRepository;
+        }
+        //O que faz
+        public void DeleteById(int id)
+        {
+            _repository.DeleteById(id);
+        }
+
+        public List<Ingredient> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Ingredient GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public Ingredient Presist(Ingredient ingredient)
+        {
+            return _repository.Presist(ingredient);
+        }
+
+        public Ingredient Update(Ingredient ingredient)
+        {
+            return _repository.Update(ingredient);
+        }
+    }
+}
