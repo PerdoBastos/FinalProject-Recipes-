5d257b4650971288564dc43e157ec7ca8ade0d0b
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-18 20:05:44 +0100

    measure service

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Service/Implementation/MeasureService.cs
@@ -0,0 +1,39 @@
+﻿using AssemblyCookieRecipe.Model;
+using AssemblyCookieRecipe.Repository.Interfaces;
+using AssemblyCookieRecipe.Service.Interfaces;
+
+namespace AssemblyCookieRecipe.Service.Implementation
+{
+    public class MeasureService : IMeasureService
+    {
+        private readonly IMeasureRepository _repository;
+        public MeasureService(IMeasureRepository measureRepository)
+        {
+            _repository = measureRepository;
+        }
+        public void DeleteById(int id)
+        {
+            _repository.DeleteById(id);
+        }
+
+        public List<Measure> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Measure GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public Measure Presist(Measure measure)
+        {
+            return _repository.Presist(measure);
+        }
+
+        public Measure Update(Measure measure)
+        {
+            return _repository.Update(measure);
+        }
+    }
+}
