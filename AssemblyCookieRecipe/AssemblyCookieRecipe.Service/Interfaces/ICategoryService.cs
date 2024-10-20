4df160d14c610f654bfde3668c3bce1dad95fa57
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-20 08:21:27 +0100

    Icategoryservice

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Service/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using AssemblyCookieRecipe.Model;
+
+namespace AssemblyCookieRecipe.Service.Interfaces
+{
+    public interface ICategoryService
+    {
+        Category Presist(Category category);
+        List<Category> GetAll();
+        Category GetById(int id);
+        Category Update(Category category);
+        void DeleteById(int id);
+    }
+}
