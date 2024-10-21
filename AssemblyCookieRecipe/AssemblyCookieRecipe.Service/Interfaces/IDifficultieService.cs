0e1c7ba4059be97cb01f6f6d25dc411f28d09b53
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-21 21:11:05 +0100

    idifficulties

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Service/Interfaces/IDifficultieService.cs
@@ -0,0 +1,13 @@
+﻿using AssemblyCookieRecipe.Model;
+
+namespace AssemblyCookieRecipe.Service.Interfaces
+{
+    public interface IDifficultieService
+    {
+        Difficultie Presist(Difficultie difficultie);
+        List<Difficultie> GetAll();
+        Difficultie GetById(int id);
+        Difficultie Update(Difficultie difficultie);
+        void DeleteById(int id);
+    }
+}
