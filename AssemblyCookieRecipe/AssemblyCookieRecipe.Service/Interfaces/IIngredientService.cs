e0d18098a19aa119f8d31b5c985e1bad7cc07920
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-22 19:18:29 +0100

    IIngedientService

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Service/Interfaces/IIngredientService.cs
@@ -0,0 +1,13 @@
+﻿using AssemblyCookieRecipe.Model;
+
+namespace AssemblyCookieRecipe.Service.Interfaces
+{
+    public interface IIngredientService
+    {
+        Ingredient Presist(Ingredient ingredient);
+        List<Ingredient> GetAll();
+        Ingredient GetById(int id);
+        Ingredient Update(Ingredient ingredient);
+        void DeleteById(int id);
+    }
+}
