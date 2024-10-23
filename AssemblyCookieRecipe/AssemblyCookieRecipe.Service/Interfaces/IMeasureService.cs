d6649966a05acdcdb2d7076ac251a251c2c442c4
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-23 23:48:55 +0100

    IMeasure service

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Service/Interfaces/IMeasureService.cs
@@ -0,0 +1,13 @@
+﻿using AssemblyCookieRecipe.Model;
+
+namespace AssemblyCookieRecipe.Service.Interfaces
+{
+    public interface IMeasureService
+    {
+        Measure Presist(Measure measure);
+        List<Measure> GetAll();
+        Measure GetById(int id);
+        Measure Update(Measure measure);
+        void DeleteById(int id);
+    }
+}
