b18fd8bf606a9c364f28e6e78d86c0d333ae9a1c
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-10-24 00:44:35 +0100

    IUserService

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.Service/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using AssemblyCookieRecipe.Model;
+
+namespace AssemblyCookieRecipe.Service.Interfaces
+{
+    public interface IUserService
+    {
+        User Presist(User user);
+        List<User> GetAll();
+        User GetById(int id);
+        User Update(User user);
+        void DeleteById(int id);
+    }
+}
