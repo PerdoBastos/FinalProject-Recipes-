73694d3f7b34789cb468f2ae4eddcdf866c0a8a7
Pedro Bastos <pedro.miguel.ameixa.bastos@gmail.com>
2024-11-18 20:57:19 +0000

    getall

---
+++ AssemblyCookieRecipe/AssemblyCookieRecipe.WebApp/Pages/Measures/GetAll.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AssemblyCookieRecipe.WebApp.Pages.Measures
+{
+    public class GetAllModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
